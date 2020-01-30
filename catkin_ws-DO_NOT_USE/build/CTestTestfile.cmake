69922a65781e96b9642af0b188e884e059c03d9f
peter2380123 <peter2380123@gmail.com>
2020-01-30 11:52:18 +1000

    Removed legacy folder, added submodule ver instead
    Now catkin_ws comes in the submodule form.

--- catkin_ws-DO_NOT_USE/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/catkin_ws/src
-# Build directory: /home/peter/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("qutms_autonomous_description")
-subdirs("qutms_autonomous_gazebo")
