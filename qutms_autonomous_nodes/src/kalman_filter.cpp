b73386ecac132586f02661dea91daa4ec15d3a82
NickManjan <nickmangan2825@gmail.com>
2020-07-11 11:15:13 +1000

    Temporary KF node up

--- qutms_autonomous_nodes/src/kalman_filter.cpp
+++ qutms_autonomous_nodes/src/kalman_filter.cpp
@@ -14,6 +14,7 @@
 #include <boost/array.hpp>
 #include <iostream>
 #include <fstream>
+#include <random>
 
 
 // Implements an Adaptive Kalman Filter to estimate the state of a platform
@@ -21,12 +22,18 @@
 
 using namespace std;
 
+ofstream outfile;
+char dir[300];
+char dira[300];
+char dirb[300];
+char dirc[300];
+
 class Adaptive_Kalman_Filter {
     // KF matrices 
-    Eigen::MatrixXd A, H, P, Q, R, C; // Process matrices and covariances
+    Eigen::MatrixXd A, H, P, Pa, Q, R, C; // Process matrices and covariances
 
-    Eigen::VectorXd xk;
-    Eigen::Vector3d gl, w_en, w_ie, p_imu, v; // State vector
+    Eigen::VectorXd xk, xka; // State vector
+    Eigen::Vector3d gl, w_en, w_ie, p_imu, v; 
 
     public:
     // Constructor
@@ -37,13 +44,12 @@ class Adaptive_Kalman_Filter {
     void state_update(void);
     void cov_update(int);
 
-    void imu_meas(double, double, double, double);
-    void gps_meas(double, double);
     void state_pub(void);
     void pos_ins_prop(void);
-    void quat_2_eu(double&, double&, double&, double, double, double, double);
+    void quat_2_eu(double, double, double, double);
+    void info_process(int jj);
 
-    Eigen::Matrix3d euler_to_dcm(double, double, double);
+    void euler_to_dcm(double, double, double);
 
     void info_write(void);
 
@@ -62,7 +68,7 @@ class Adaptive_Kalman_Filter {
     geometry_msgs::TwistWithCovariance state_vel_msg;
 
     // Measurement variables
-    double latm, lngm, psim, lat_dotm, lng_dotm, psi_dotm, h_dot;
+    double latm, lngm, psim, lat_dotm, lng_dotm, psi_dotm, h_dot, phi, theta, psi;
 
     // KF measurement matrix
     Eigen::VectorXd yk, ek;
@@ -72,15 +78,27 @@ class Adaptive_Kalman_Filter {
     tf2::Quaternion quaternion;
     boost::array<double, 36UL> pose_cov;
     boost::array<double, 36UL> vel_cov;
-    double g = 9.79;
+    double g = 9.7754;
     double sigma = 7.292115E-5;
     double R0 = 6378137; 
     vector<double> att;
     Eigen::Matrix3d DCM;
-    ofstream outfile;
+    double d2r = M_PI/180;
+
+    vector<double> gps_data, imu_accel_data, imu_rate_data, att_data;
+
 };
 
-Adaptive_Kalman_Filter::Adaptive_Kalman_Filter() : A(6,6), H(6,6), P(6,6), Q(6,6), R(6,6), C(6,6), xk(6,1) {
+void SigIntHandler(int sig) {
+    // Close the opened text file, and shutdown
+    ROS_INFO("Closing opened data file...");
+    outfile.close();
+    ROS_ERROR("File closed, shutting down");
+    ros::shutdown();
+}
+
+Adaptive_Kalman_Filter::Adaptive_Kalman_Filter() : A(6,6), H(6,6), P(6,6), Pa(6,6), Q(6,6), R(6,6), C(6,6), xk(6,1), xka(6,1) {
+
     // Setup all necessary variables
     A <<  1, 0, 0, 0.1, 0, 0,
         0, 1, 0, 0, 0.1, 0,
@@ -93,7 +111,7 @@ Adaptive_Kalman_Filter::Adaptive_Kalman_Filter() : A(6,6), H(6,6), P(6,6), Q(6,6
 
     H << 1, 0, 0, 0, 0, 0,
         0, 1, 0, 0, 0, 0,
-        0, 0, 0.1, 0, 0, 0,
+        0, 0, 1, 0, 0, 0,
         0, 0, 0, 0.1, 0, 0,
         0, 0, 0, 0, 0.1, 0,
         0, 0, 0, 0, 0, 1;
@@ -102,20 +120,36 @@ Adaptive_Kalman_Filter::Adaptive_Kalman_Filter() : A(6,6), H(6,6), P(6,6), Q(6,6
 
     Q << 1.3479, 0, 0, 0, 0, 0,
         0, 1.3479, 0, 0, 0, 0,
-        0, 0, 2*M_PI/180, 0, 0, 0,
+        0, 0, 2*d2r, 0, 0, 0,
         0, 0, 0, 0.1214, 0, 0,
         0, 0, 0, 0, 0.1214, 0,
-        0, 0, 0, 0, 0, 2*M_PI/180;
+        0, 0, 0, 0, 0, 2*d2r;
 
     C = Eigen::MatrixXd::Ones(6,6);
 
-    xk << 49.0086, 8.3981, 2.6006, 0.6984, -0.0183, -0.0062;
+    // Initialise the state and other vectors
+    xk << 49.0086*d2r, 8.3981*d2r, 2.6006, 0.6984, -0.0183, -0.0062;
 
-    gl << 0, 0, g;
-    w_en << 0, 0, 0;
-    w_ie << sigma*cos(xk[0]*M_PI/180), 0, -sigma*sin(xk[0]*M_PI/180);
-    p_imu << 49.0086448, 8.3981040, 112.9905930;
-    v << 0.6984, -0.0183, -0.0021;    
+    gl(0) = -0.0168; 
+    gl(1) = 0; 
+    gl(2) = g;
+    
+    w_en(0) = 0; 
+    w_en(1) = 0; 
+    w_en(2) = 0;
+    
+    w_ie(0) = sigma*cos(xk(0)); 
+    w_ie(1) = 0; 
+    w_ie(2) = -sigma*sin(xk(0));
+    
+    p_imu(0) = 49.0086448*d2r; 
+    p_imu(1) = 8.3981040*d2r; 
+    p_imu(2) = 112.9905930;
+
+    v(0) = 0.6984; 
+    v(1) = -0.0183; 
+    v(2) = -0.0021;    
+    
     // Initialise the sizes of the other important vectors
     yk.resize(6,1);
 
@@ -124,23 +158,101 @@ Adaptive_Kalman_Filter::Adaptive_Kalman_Filter() : A(6,6), H(6,6), P(6,6), Q(6,6
     state_vel_pub = na.advertise<geometry_msgs::TwistWithCovariance>("/qev2_state_vel", 10);
 
     // Subscribers
-    imu_sub = na.subscribe("/qev2/imu/data", 10, &Adaptive_Kalman_Filter::imu_callback, this);
-    gps_sub = na.subscribe("/qev2/gps/data", 10, &Adaptive_Kalman_Filter::gps_callback, this);  
+    imu_sub = na.subscribe("/KITTI_imu", 10, &Adaptive_Kalman_Filter::imu_callback, this);
+    gps_sub = na.subscribe("/KITTI_gps", 10, &Adaptive_Kalman_Filter::gps_callback, this);  
+
+    // Get the custom shutdown handler
+    signal(SIGINT, SigIntHandler);
+
+    // Setup for rand generator
+    const double mean = 0;
+    const double sigma = 0.5;
+    auto dist = std::bind(std::normal_distribution<double>{mean, sigma},std::mt19937(std::random_device{}()));
+
+    // Get the GPS file
+    sprintf(dir, "/home/nick-hp-ubuntu/Documents/Text Files/KITTI_gps.txt");
+
+    // Process the text file
+    string line;
+    ifstream infile (dir);
+    if(infile.is_open()) {
+        while(getline(infile, line)) {
+            double num10, num11, num12;
+            infile >> num10 >> num11 >> num12;
+            // cout << "Got " << num1 << " " << num2 << " " << num3 << endl;
+            gps_data.push_back((num10 + dist())*d2r);
+            gps_data.push_back((num11 + dist())*d2r);
+            gps_data.push_back((num12 + dist())*d2r);
+        }
+    }
+    infile.close();
+
+        // Get 2 directories to a text file
+    sprintf(dira, "/home/nick-hp-ubuntu/Documents/Text Files/KITTI_accel_xyz.txt");
+    sprintf(dirb, "/home/nick-hp-ubuntu/Documents/Text Files/KITTI_brate_xyz.txt");
+    sprintf(dirc, "/home/nick-hp-ubuntu/Documents/Text Files/KITTI_att.txt");
+
+    // Process the text file for acceleration data
+    string aline;
+    ifstream ainfile (dira);
+    if(ainfile.is_open()) {
+        while(getline(ainfile, aline)) {
+            double num1, num2, num3;
+            ainfile >> num1 >> num2 >> num3;
+            // cout << "Got " << num1 << " " << num2 << " " << num3 << endl;
+            imu_accel_data.push_back(num1 + dist());
+            imu_accel_data.push_back(num2 + dist());
+            imu_accel_data.push_back(num3 + dist());
+        }
+    }
+    ainfile.close();
+
+    // Open up and process the next file
+    string rline;
+    ifstream rinfile (dirb);
+    if(rinfile.is_open()) {
+        while(getline(rinfile, rline)) {
+            double num4, num5, num6;
+            rinfile >> num4 >> num5 >> num6;
+            // cout << "Got " << num4 << " " << num5 << " " << num6 << endl;
+            imu_rate_data.push_back(num4 + dist());
+            imu_rate_data.push_back(num5 + dist());
+            imu_rate_data.push_back(num6 + dist());
+        }
+    }
+    rinfile.close();
+
+    // Process attitude data
+    string tline;
+    ifstream tinfile (dirc);
+    if(tinfile.is_open()) {
+        while(getline(tinfile, tline)) {
+            double num7, num8, num9;
+            tinfile >> num7 >> num8 >> num9;
+            // cout << "Got " << num7 << " " << num8 << " " << num9 << endl;
+            att_data.push_back(num7 + dist());
+            att_data.push_back(num8 + dist());
+            att_data.push_back(num9 + dist());
+        }
+    }
+    tinfile.close();
 
     // Open a text file
-    outfile.open("kf_results.txt");  
+    outfile.open("/home/nick-hp-ubuntu/catkin_ws/src/qutms_autonomous_nodes/kf_results.txt");
+
+    // Time to get to here: 0.013s
 
 }
 
 void Adaptive_Kalman_Filter::state_predict(void) {
     // Predict the next state
-    xk = A*xk; 
+    xka = A*xk; 
 
     // Predict the next covariance
     P = A*P*A.transpose() + Q;
 
     // DEBUG
-    cout << "The new state is: " << xk << endl;
+    // cout << "The new state is: " << xk << endl;
 }
 
 void Adaptive_Kalman_Filter::state_update(void) {
@@ -149,14 +261,15 @@ void Adaptive_Kalman_Filter::state_update(void) {
     K = P*H.transpose()*residmat.inverse();
 
     // DEBUG
-    cout << "Calculated Kalman Gain as: " << K << endl;
+    // cout << "Calculated Kalman Gain as: " << K << endl;
 
     // State update, assuming we have measurements
+    // ROS_INFO("Passing %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f to measurement vector", latm, lngm, psim, lat_dotm, lng_dotm, psi_dotm);
     yk << latm, lngm, psim, lat_dotm, lng_dotm, psi_dotm;
 
     // DEBUG
     // cout << "Got measurements: " << yk << endl;
-    xk = xk + K*(yk - H*xk);
+    xk = xka + K*(H*yk - H*xka);
 
     // DEBUG
     cout << "The updated state is: " << xk << endl;
@@ -165,21 +278,16 @@ void Adaptive_Kalman_Filter::state_update(void) {
     P = (Eigen::MatrixXd::Identity(6,6) - K*H)*P;
 
     // Get the error matrix
-    ek = xk - yk;
+    ek = H*yk - H*xk;
 }
 
 void Adaptive_Kalman_Filter::cov_update(int k) {
     // Update the state and measurement covariance matrices
     // Innovation covariance matrix
     Eigen::MatrixXd Ck;
-    // Sanity check - the error matrix exists
-    if (ek.rows() != 0) {
-        Ck = ((k-1)/k)*C + (1/k)*ek*ek.transpose();
-    } else {
-        ROS_ERROR("Error matrix size is 0, defaulting to ones");
-        ek = Eigen::VectorXd::Ones(6,1);
-        Ck = ((k-1)/k)*C + (1/k)*ek*ek.transpose();
-    }
+
+    // Calculate C using the error matrix
+    Ck = ((k-1)/k)*C + (1/k)*ek*ek.transpose();
 
     // Update measurement covariance
     R = Ck + H*P*H.transpose();
@@ -195,50 +303,47 @@ void Adaptive_Kalman_Filter::pos_ins_prop(void) {
     // Propagates acceleration measurements to estimate position
 
     // Transform acceleration values
-    Eigen::Vector3d fbc(1,3), fnc;
+    Eigen::Vector3d fbc;
+    fbc.resize(3,1);
     fbc << lat_dotm, lng_dotm, h_dot;
-    fnc = DCM*fbc;
+    Eigen::VectorXd fnc = DCM*fbc;
+    // cout << fnc << endl;
 
     // Propagate velocity
-    Eigen::VectorXd v_dot = fnc - v.cross(w_ie);
+    Eigen::Vector3d w_ie_t = 2*w_ie.transpose() + w_en.transpose();
+    Eigen::VectorXd v_dot = fnc - w_ie_t.cross(v) + gl;
     v = v + v_dot*0.1; 
 
+    // cout << "Velocity and acceleration is: " << v << "\n" << v_dot << endl;
+    double vx = v(0);
+    double vy = v(1);
+    double vz = v(2);
+    double xt = p_imu(0);
+    double yt = p_imu(1);
+    double zt = p_imu(2);
+
     // Get velocity values in lat and long
-    double Ldot = v[0]/(R0 + p_imu[2]);
-    double ldot = v[1]/(cos(p_imu[0]*M_PI/180)*(R0 + p_imu[2]));
-    double hdot = -v[2];
+    double Ldot = vx/(R0 + zt);
+    double ldot = vy*(1/(cos(xt))/(R0 + zt));
+    double hdot = -vz;
 
     // Update position
-    p_imu[0] = p_imu[0] + Ldot*0.1;
-    p_imu[1] = p_imu[1] + ldot*0.1;
-    p_imu[2] = p_imu[2] + hdot*0.1;
+    latm = xt + Ldot*0.1;
+    lngm = yt + ldot*0.1;
+    double hm = zt + hdot*0.1;
 
     // Measurement update
-    latm = p_imu[0];
-    lngm = p_imu[1];
+    p_imu << latm, lngm, hm;
+
+    // cout << "The new IMU position is: " << p_imu << endl;
 
 }
 
-void Adaptive_Kalman_Filter::imu_meas(double psii, double lat_doti, double lng_doti, double psi_doti) {
-    // Update measurements from an IMU
-    psim = psii;
-    lat_dotm = lat_doti;
-    lng_dotm = lng_doti;
-    psi_dotm = psi_doti;
-
-}
-
-void Adaptive_Kalman_Filter::gps_meas(double lati, double lngi) {
-    // Update measurements from a GPS
-    latm = lati;
-    lngm = lngi; 
-}
-
-void Adaptive_Kalman_Filter::quat_2_eu(double& r, double& p, double& ya, double x, double y, double z, double w) {
+void Adaptive_Kalman_Filter::quat_2_eu(double x, double y, double z, double w) {
     // Convert a quaternion (x, y, z, w) represenation to an rpy representation
-    r = atan2((2*(y*z + x*w)),(x*x + y*y - z*z - w*w));
-    p = asin(-2*(y*w - x*z));
-    ya = atan2((2*(z*w + x*y)),(x*x - y*y - z*z + w*w));
+    psi = atan2((2*(y*z + x*w)),(x*x + y*y - z*z - w*w));
+    theta = asin(-2*(y*w - x*z));
+    phi = atan2((2*(z*w + x*y)),(x*x - y*y - z*z + w*w));
 
 }
 
@@ -272,17 +377,38 @@ void Adaptive_Kalman_Filter::state_pub(void) {
     state_vel_pub.publish(state_vel_msg);
 }
 
-Eigen::Matrix3d Adaptive_Kalman_Filter::euler_to_dcm(double r, double p, double y) {
+void Adaptive_Kalman_Filter::info_process(int jj) {
+    // Dummy function to imitate subscribers receiving information
+    // Grabs information from vectors and passes it to sensor information vector
+
+    // Assign IMU data
+    lat_dotm = imu_accel_data[jj];
+    lng_dotm = imu_accel_data[jj+1];
+    h_dot = imu_accel_data[jj+2];
+    psi_dotm = imu_rate_data[jj+2];
+    psim = att_data[jj+2];
+
+    // Get a DCM
+    euler_to_dcm(att_data[jj], att_data[jj+1], att_data[jj+2]);
+
+    // Check once 10 iterations has been done, and assign the GPS data
+    pos_ins_prop(); // Propagate position with INS mechanisation
+    int iteration = jj % 10;
+    if (!iteration) {
+        latm = gps_data[jj];
+        lngm = gps_data[jj+1];
+        p_imu << latm, lngm, gps_data[jj+2];
+    }
+}
+
+void Adaptive_Kalman_Filter::euler_to_dcm(double r, double p, double y) {
     // Transform an RPY representation to a DCM
-    Eigen::Matrix3d dcm;
 
-    double d2r = M_PI/180;
+    DCM << cos(p)*cos(y), -cos(r)*sin(y)+sin(r)*sin(p)*cos(y), sin(r)*sin(y)+cos(r)*sin(p)*cos(y),
+            cos(p)*sin(y), cos(r)*cos(y)+sin(r)*sin(p)*sin(y), -sin(r)*cos(y)+cos(r)*sin(p)*sin(y),
+            -sin(p), sin(r)*cos(p), cos(r)*cos(p);
 
-    dcm << cos(p*d2r)*cos(y*d2r), -cos(r*d2r)*sin(y*d2r)+sin(r*d2r)*sin(p*d2r)*cos(y*d2r), sin(r*d2r)*sin(y*d2r)+cos(r*d2r)*sin(p*d2r)*cos(y*d2r),
-            cos(p*d2r)*sin(y*d2r), cos(r*d2r)*cos(y*d2r)+sin(r*d2r)*sin(p*d2r)*sin(y*d2r), -sin(r*d2r)*cos(y*d2r)+cos(r*d2r)*sin(p*d2r)*sin(y*d2r),
-            -sin(p*d2r), sin(r*d2r)*cos(p*d2r), cos(r*d2r)*cos(p*d2r);
-    
-    return dcm;
+    // cout << "Calculated DCM " << DCM << endl;
 
 }
 
@@ -292,9 +418,9 @@ void Adaptive_Kalman_Filter::info_write(void) {
     if(outfile.is_open()) {
         // Get the updated state and Kalman Gain matrix
         outfile << xk << "\n";
-        outfile << K << "\n";
+        // outfile << K << "\n";
     } else {
-        ROS_ERROR("File not opened");
+        ROS_ERROR("File not opened, unable to write information");
     }
 
 }
@@ -305,19 +431,22 @@ void Adaptive_Kalman_Filter::imu_callback(const sensor_msgs::Imu::ConstPtr& imu_
     double y = imu_msg->orientation.y;
     double z = imu_msg->orientation.z;
     double w = imu_msg->orientation.w;
-    double lat_dot = imu_msg->linear_acceleration.x;
-    double lng_dot = imu_msg->linear_acceleration.y;
-    double psi_dot = imu_msg->angular_velocity.z;
+    Adaptive_Kalman_Filter::lat_dotm = imu_msg->linear_acceleration.x;
+    Adaptive_Kalman_Filter::lng_dotm = imu_msg->linear_acceleration.y;
+    Adaptive_Kalman_Filter::h_dot = imu_msg->linear_acceleration.z;
+    Adaptive_Kalman_Filter::psi_dotm = imu_msg->angular_velocity.z;
+    // double lat_dot = imu_msg->linear_acceleration.x;
+    // double lng_dot = imu_msg->linear_acceleration.y;
+    // double psi_dot = imu_msg->angular_velocity.z;
 
     // Convert quaternion to rpy
-    double phi, theta, psi;
-    quat_2_eu(phi, theta, psi, x, y, z, w);
-
-    // Update
-    imu_meas(psi, lat_dot, lng_dot, psi_dot);
+    quat_2_eu(x, y, z, w);
 
     // Get a DCM
-    DCM = euler_to_dcm(phi, theta, psi);
+    euler_to_dcm(phi, theta, psi);
+
+    // Propagate position with the IMU data
+    pos_ins_prop();
 }
 
 void Adaptive_Kalman_Filter::gps_callback(const sensor_msgs::NavSatFix::ConstPtr& gps_msg) {
@@ -326,44 +455,51 @@ void Adaptive_Kalman_Filter::gps_callback(const sensor_msgs::NavSatFix::ConstPtr
     double lng = gps_msg->longitude;
     double h = gps_msg->altitude;
 
+    // Update
+    Adaptive_Kalman_Filter::latm = lat;
+    Adaptive_Kalman_Filter::lngm = lng;
+    
     // Update the p_imu variable
     p_imu << lat, lng, h;
-
-    // Update
-    gps_meas(lat, lng);
 }
 
 int main(int argc, char **argv) {
     // Init node
-    ros::init(argc, argv, "QEV2_AKF");
+    ros::init(argc, argv, "QEV2_AKF", ros::init_options::NoSigintHandler);
 
     // Instantiate class
     Adaptive_Kalman_Filter AKF;
 
-    // Run as fast as the quickest subscriber
+    // Run as fast as the quickest publisher
     ros::Rate rate(1);
 
     // Get a counter
     int f = 1;
-    while(ros::ok()) {
+    int n = 3;
+    while((ros::ok()) && (f != 999)) {
 
         // State prediction
         AKF.state_predict();
 
         // Process any waiting callbacks
-        ros::spinOnce();
+        // ros::spinOnce();
+        AKF.info_process(n);
 
         // State update
         AKF.state_update();
 
-        // Update our covariances
-        //AKF.cov_update(f);
+        // Update the covariances
+        // AKF.cov_update(f);
 
         // Publish the state
         AKF.state_pub();
 
         // Increment
         f++;
+        n = n + 3;
+
+        // Save the required information
+        AKF.info_write();
 
         // Sleep
         rate.sleep();
