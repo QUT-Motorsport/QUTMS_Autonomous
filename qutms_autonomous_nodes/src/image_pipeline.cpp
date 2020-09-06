4b3b5b4665cac4f6d196226c3d46f1d2f3119f2d
nick-pc-ubuntu@nickpcubuntu-System-Product-Name.NickManjan <nick-pc-ubuntu@nickpcubuntu-System-Product-Name.nickmangan2825@gmail.com>
2020-09-06 17:59:35 +1000

    Added gain for model controller node

--- qutms_autonomous_nodes/src/image_pipeline.cpp
+++ qutms_autonomous_nodes/src/image_pipeline.cpp
@@ -1,6 +1,6 @@
 #include <ros/ros.h>
 #include <std_msgs/Int64.h>
-#include <geometry_msgs/Point.h>
+#include <std_msgs/Float64.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CompressedImage.h>
@@ -18,8 +18,8 @@
 // IMPORTANT: use associated launch file for testing
 
 using namespace std;
-static const std::string OPENCV_WINDOW_LEFT = "Image_Window_Left";
-static const std::string OPENCV_WINDOW_RIGHT = "Image_Window_Right";
+static const string OPENCV_WINDOW_LEFT = "Image_Window_Left";
+static const string OPENCV_WINDOW_RIGHT = "Image_Window_Right";
 
 class QEV_Image {
 
@@ -28,6 +28,7 @@ class QEV_Image {
     
     void image_left_callback(const sensor_msgs::Image::ConstPtr& img_left_msg);
     void image_right_callback(const sensor_msgs::Image::ConstPtr& img_right_msg);
+    void pos_gain(void);
 
     private:
     ros::NodeHandle ni;
@@ -37,11 +38,14 @@ class QEV_Image {
     ros::Publisher point_pub;
     ros::Publisher lap_pub; // Might be replaced with a param server function
     std_msgs::Int64 lap_msg;
-    geometry_msgs::Point point_msg;
+    std_msgs::Float64 gain_msg;
     string image_transport_param;
 
     // Cluster point values
     int b_left_x, b_left_y, y_left_x, y_left_y, b_right_x, b_right_y, y_right_x, y_right_y;
+    double im_left_x, im_right_x, im_left_y, im_right_y, left_p_dist, right_p_dist, steer_gain;
+
+    bool no_b_left, no_y_left, no_b_right, no_y_right;
 
     // Image segmentation values
     // Yellow values
@@ -67,6 +71,12 @@ class QEV_Image {
     int b_iLastX = -1; 
     int b_iLastY = -1;
 
+    // Image resizing values
+    int im_size_top = 600;
+    int im_size_bottom = 1080;
+    int im_size_left = 1020;
+    int im_size_right = 2820; 
+
 
 };
 
@@ -83,7 +93,7 @@ void SigIntHandler(int sig) {
 QEV_Image::QEV_Image(): imi(ni) {
 
     // Publishers here
-    point_pub = ni.advertise<geometry_msgs::Point>("/qev/move_point", 10);
+    point_pub = ni.advertise<std_msgs::Float64>("/qev/move_point", 10);
     lap_pub = ni.advertise<std_msgs::Int64>("/qev/lap_count", 10);
 
     // Subscribers here
@@ -127,19 +137,18 @@ void QEV_Image::image_left_callback(const sensor_msgs::Image::ConstPtr& img_left
     // Threshold blue
     cv::inRange(im_left_hsv, cv::Scalar(b_low_h,b_low_s,b_low_v), cv::Scalar(b_high_h, b_high_s, b_high_v), im_thres_b_left);
 
+    // Remove small objects
+    cv::erode(im_thres_y_left, im_thres_y_left, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));
+    cv::dilate(im_thres_y_left, im_thres_y_left, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));     
+    cv::erode(im_thres_b_left, im_thres_b_left, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));
+    cv::dilate(im_thres_b_left, im_thres_b_left, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));     
+
     // Moments
     cv::Moments im_left_ymoments = cv::moments(im_thres_y_left);
     cv::Moments im_left_bmoments = cv::moments(im_thres_b_left);
 
     // Recreate an image
     cv::Mat im_thres_left = im_thres_b_left + im_thres_y_left;
-    
-    // Reindex 
-    im_thres_left = im_thres_left(cv::Range(600, 1080), cv::Range(1020, 2580));
-
-    // Remove small objects
-    cv::erode(im_thres_left, im_thres_left, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-    cv::dilate(im_thres_left, im_thres_left, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));     
 
     // Check for blue and yellow detections
     int b_left_cnt = 0; 
@@ -160,13 +169,22 @@ void QEV_Image::image_left_callback(const sensor_msgs::Image::ConstPtr& img_left
         }
     }
 
+    // ROS_INFO("Number of nonzero blue elements: %d", b_left_cnt);
+    // ROS_INFO("Number of nonzero yellow elements: %d", y_left_cnt);
+
     // Notify if no detections are present
     if(b_left_cnt == 0) {
         ROS_ERROR("No blue cones detected!!!");
+        no_b_left = true;
+    } else {
+        no_b_left = false;
     }
     
     if(y_left_cnt == 0) {
         ROS_ERROR("No yellow cones detected!!!");
+        no_y_right = true;
+    } else {
+        no_y_left = false;
     }
 
     // Grab the image moment values
@@ -187,7 +205,25 @@ void QEV_Image::image_left_callback(const sensor_msgs::Image::ConstPtr& img_left
         y_left_x = im_y_left_m10/im_y_left_area;
         y_left_y = im_y_left_m01/im_y_left_area;
     }
+
     // ROS_INFO("Calculated left point pairs are: (%d, %d), (%d, %d)", b_left_x, b_left_y, y_left_x, y_left_y);
+
+    // Assign the found points new values to differentiate
+    im_thres_left.at<uchar>(cv::Point(b_left_x,b_left_y)) = 255;
+    im_thres_left.at<uchar>(cv::Point(y_left_x,y_left_y)) = 255;    
+
+    // Get the midpoint
+    im_left_x = round((b_left_x+y_left_x)/2);
+    im_left_y = round((b_left_y+y_left_y)/2);
+
+    // Show on image
+    im_thres_left.at<uchar>(cv::Point(im_left_x, im_left_y)) = 255;
+
+    // Get point distance from centre
+    left_p_dist = im_thres_left.cols/2 - im_left_x;
+
+    // Resize 
+    im_thres_left = im_thres_left(cv::Range(im_size_top, im_size_bottom), cv::Range(im_size_left, im_size_right));
 
     // Display
     cv::imshow(OPENCV_WINDOW_LEFT, im_thres_left);
@@ -219,6 +255,12 @@ void QEV_Image::image_right_callback(const sensor_msgs::Image::ConstPtr& img_rig
 
     // Threshold blue
     cv::inRange(im_right_hsv, cv::Scalar(b_low_h,b_low_s,b_low_v), cv::Scalar(b_high_h, b_high_s, b_high_v),im_thres_b_right);
+
+    // Remove small objects
+    cv::erode(im_thres_y_right, im_thres_y_right, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));
+    cv::dilate(im_thres_y_right, im_thres_y_right, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12))); 
+    cv::erode(im_thres_b_right, im_thres_b_right, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));
+    cv::dilate(im_thres_b_right, im_thres_b_right, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(12, 12)));     
     
     // Moments
     cv::Moments im_right_ymoments = cv::moments(im_thres_y_right);
@@ -226,13 +268,6 @@ void QEV_Image::image_right_callback(const sensor_msgs::Image::ConstPtr& img_rig
 
     // Rejoin the images
     cv::Mat im_thres_right = im_thres_b_right + im_thres_y_right;
-
-    // Reindex 
-    im_thres_right = im_thres_right(cv::Range(600, 1080), cv::Range(1020, 2580));
-
-    // Remove small objects
-    cv::erode(im_thres_right, im_thres_right, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-    cv::dilate(im_thres_right, im_thres_right, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5))); 
 
     // Check for blue and yellow detections
     int b_right_cnt = 0; 
@@ -253,13 +288,22 @@ void QEV_Image::image_right_callback(const sensor_msgs::Image::ConstPtr& img_rig
         }
     }
 
+    // ROS_INFO("Number of nonzero blue elements: %d", b_right_cnt);
+    // ROS_INFO("Number of nonzero blue elements: %d", y_right_cnt);
+
     // Notify if no detections are present
     if(b_right_cnt == 0) {
         ROS_ERROR("No blue cones detected!!!");
+        no_b_right = true;
+    } else {
+        no_b_right = false;
     }
     
     if(y_right_cnt == 0) {
         ROS_ERROR("No yellow cones detected!!!");
+        no_y_right = true;
+    } else {
+        no_y_right = false;
     }
     
     // Grab the image moment values
@@ -280,11 +324,55 @@ void QEV_Image::image_right_callback(const sensor_msgs::Image::ConstPtr& img_rig
         y_right_x = im_y_right_m10/im_y_right_area;
         y_right_y = im_y_right_m01/im_y_right_area;
     }
+
     // ROS_INFO("Calculated right point pairs are: (%d, %d), (%d, %d)", b_right_x, b_right_y, y_right_x, y_right_y);
+
+    // Assign the found points new values to differentiate
+    im_thres_right.at<uchar>(cv::Point(b_right_x,b_right_y)) = 255;
+    im_thres_right.at<uchar>(cv::Point(y_right_x,y_right_y)) = 255;    
+
+    // Get the midpoint
+    im_right_x = round((b_right_x+y_right_x)/2);
+    im_right_y = round((b_right_y+y_right_y)/2);
+
+    // Show on image
+    im_thres_right.at<uchar>(cv::Point(im_right_x, im_right_y)) = 255;
+
+    // Get distance from image centre
+    right_p_dist = im_thres_right.cols/2 - im_right_x;
+
+    // Resize
+    im_thres_right = im_thres_right(cv::Range(im_size_top, im_size_bottom), cv::Range(im_size_left, im_size_right));
 
     // Display
     cv::imshow(OPENCV_WINDOW_RIGHT, im_thres_right); 
 
+}
+
+void QEV_Image::pos_gain(void) {
+    // Get the average of the two produced positions and convert to a gain value
+    // DEBUG: Function can access the values properly
+    ROS_INFO("Offset values are: %2.3f, %2.3f", left_p_dist, right_p_dist);
+    double x_off = (left_p_dist + right_p_dist)/2;
+
+    // Get the gain
+    steer_gain = x_off/100;
+
+    // Check: no blue cones
+    if((no_b_left) && (no_b_right)) {
+        // Turn left until a cone is found
+        steer_gain = 1;
+    }
+
+    // Check: no yellow cones
+    if((no_y_left) && (no_y_right)) {
+        // Turn right until a cone is found
+        steer_gain = -1;
+    }
+    
+    // Publish
+    gain_msg.data = steer_gain;
+    point_pub.publish(gain_msg);
 }
 
 int main(int argc, char **argv) {
@@ -295,16 +383,20 @@ int main(int argc, char **argv) {
     QEV_Image qev_image;
 
     // Rate set
-    // ros::Rate rate(1);
+    ros::Rate rate(5);
 
-    // // Enter while loop hereim_left_moments
-    // while(ros::ok()) {
-    //     // Spin to get some images
-    //     ros::spinOnce();
+    // Enter while loop here
+    while(ros::ok()) {
+        // Spin to get and process images
+        ros::spinOnce();
 
-    //     rate.sleep();
-    // }
+        // Get the gain value
+        qev_image.pos_gain();
 
-    ros::spin();
+        // Sleep
+        rate.sleep();
+    }
+
+    // ros::spin();
 
 }
